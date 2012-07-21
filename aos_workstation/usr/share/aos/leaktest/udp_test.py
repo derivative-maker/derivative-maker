2fb6b25e1ad18f52ecf5833c7e833e436e5a3c14
adrelanos <invalid@invalid>
2012-07-21 17:03:36 +0000

    Many changes. Separated more files. Gateway and Workstation IPs changed to avoid confusion.

---
+++ aos_workstation/usr/share/aos/leaktest/udp_test.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+# This file gets overwritten when issuing TorBOX-Workstation -update.
+
+import sys
+from scapy.all import *
+
+#define the target gateway & data payload
+target = "google.com"
+data = "testing"
+
+#define packets
+ip = IP()
+udp = UDP()
+
+#define packet parameters
+ip.dst = target
+
+#loop through all TCP ports
+for udp_port in range(0,65535):
+        udp.dport = udp_port
+        send(ip/udp/data)
