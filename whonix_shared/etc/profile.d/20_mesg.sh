e9004d77fdb6acafc57592249d154d67e24e06fe
adrelanos <adrelanos at riseup dot net>
2013-04-10 14:06:52 +0000

    comments

--- whonix_shared/etc/profile.d/20_mesg.sh
+++ whonix_shared/etc/profile.d/20_mesg.sh
@@ -4,13 +4,18 @@
 
 ## Gets run with any login shell.
 
+## Debugging
 #set -x
-
+#echo " "
+#ls -la /dev/tty
 #mesg
 
 ## Allow messages to tty
 mesg y
 
+## Debugging
 #mesg
+#sleep 20
+#set +x
 
 ## End of Whonix /etc/profile.d/20_mesg.sh
