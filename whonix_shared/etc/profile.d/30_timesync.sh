a58c40875224f834a26c196d44ac811912777308
adrelanos <adrelanos at riseup dot net>
2013-06-16 22:29:41 +0000

    whonixcheck, timsync: inform at cli, if we are still waiting for whonixcheck and/or timesync's results

--- whonix_shared/etc/profile.d/30_timesync.sh
+++ whonix_shared/etc/profile.d/30_timesync.sh
@@ -5,6 +5,11 @@
 if [ ! "$(tty)" = "/dev/tty1" ]; then
    return 0
 else
+
+   if [ ! -f /var/run/whonix/whonixcheck/timesync_done ]; then
+      echo "Waiting for results from Network Time Synchronization..."
+   fi
+   
    /usr/lib/whonix/delay timesync --showcli &
 fi
 
--- whonix_shared/etc/profile.d/40_whonixcheck.sh
+++ whonix_shared/etc/profile.d/40_whonixcheck.sh
@@ -5,6 +5,11 @@
 if [ ! "$(tty)" = "/dev/tty1" ]; then
    return 0
 else
+
+   if [ ! -f /var/run/whonix/whonixcheck/whonixcheck_done ]; then
+      echo "Waiting for results from Whonix Check..."
+   fi
+   
    /usr/lib/whonix/delay whonixcheck --showcli &
 fi
 
