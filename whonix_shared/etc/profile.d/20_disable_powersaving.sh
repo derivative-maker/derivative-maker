880b9bcbcf1689593afa184766ab8b1d394c6fb6
adrelanos <adrelanos at riseup dot net>
2013-06-26 21:56:17 +0000

    new way to find out locally using Whonix version (now using dpkg-query); automatically bumping upstream version of Whonix, no longer bumping deb revision; deleted whonix_shared/usr/share/whonix/version (no longer required); fix

--- whonix_shared/etc/profile.d/20_disable_powersaving.sh
+++ whonix_shared/etc/profile.d/20_disable_powersaving.sh
@@ -2,19 +2,15 @@
 
 ## Whonix /etc/profile.d/20_disable_powersaving.sh
 
-if [ ! "$(tty)" = "/dev/tty1" ]; then
-   return 0
-else
-   ## VMONLY
-   ## Disable monitor power saving.
-   ## Only useful for VMs.
-   ## Should be deactivated on bare metal.
+## VMONLY
+## Disable monitor power saving.
+## Only useful for VMs.
+## Should be deactivated on bare metal.
 
-   #set -x
-   USERNAME="user"
-   setterm -blank 0 -powerdown 0
-   sudo -u "$USERNAME" setterm -blank 0 -powerdown 0
-   #set +x
-fi
+#set -x
+USERNAME="user"
+setterm -blank 0 -powerdown 0
+sudo -u "$USERNAME" setterm -blank 0 -powerdown 0
+#set +x
 
 ## End of Whonix changes to /etc/profile.d/20_disable_powersaving.sh
