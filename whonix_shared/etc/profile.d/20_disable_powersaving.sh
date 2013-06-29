0df9c363511a94afe514ba06a0b196134b289684
adrelanos <adrelanos at riseup dot net>
2013-06-29 17:23:29 +0000

    only disable powersaving if a virtual machine can be detected

--- whonix_shared/etc/profile.d/20_disable_powersaving.sh
+++ whonix_shared/etc/profile.d/20_disable_powersaving.sh
@@ -2,15 +2,26 @@
 
 ## Whonix /etc/profile.d/20_disable_powersaving.sh
 
-## VMONLY
-## Disable monitor power saving.
-## Only useful for VMs.
-## Should be deactivated on bare metal.
-
 #set -x
+
+result="$(sudo virt-what)"
+
+if [ "$result" = "" ]; then
+   ## Not running in a Virtual Machine (or none detected),
+   ## therefore not disabling monitor power saving.
+   return 0
+fi
+
+## Disable monitor power saving.
+## Only useful inside Virtual Machines.
+## Monitor power saving inside Virtual Machines is not useful, only confusing
+## and does not safe any energy. That is something the host should do, if
+## wanted.
+
 USERNAME="user"
 setterm -blank 0 -powerdown 0
 sudo -u "$USERNAME" setterm -blank 0 -powerdown 0
+
 #set +x
 
 ## End of Whonix changes to /etc/profile.d/20_disable_powersaving.sh
