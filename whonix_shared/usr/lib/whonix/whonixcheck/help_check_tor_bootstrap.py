353e1cc2b47c450c4c31bea1d5bb7a8f47bc56d7
adrelanos <adrelanos at riseup dot net>
2013-08-07 07:51:14 +0000

    - No longer abort whonixcheck if Tor's SocksPort isn't reachable (#49). Now  directly checking Tor's bootstrap status through Tor's control port. (#57) - Work on #57.

--- whonix_shared/usr/lib/whonix/whonixcheck/help_check_tor_bootstrap.py
+++ whonix_shared/usr/lib/whonix/whonixcheck/help_check_tor_bootstrap.py
@@ -12,26 +12,45 @@ elif os.path.exists("/usr/share/whonix/whonix_workstation"):
   ## Control Port Filter Proxy listens on 9052 on Whonix-Gateway
   p=9052
 else:
-  sys.exit(-1);
+  exit_code=254
+  sys.exit(exit_code)
 
-with Controller.from_port(port = p) as controller:
-  ## Authentication not necessary when using Control Port Filter Proxy.
-  #controller.authenticate("password")
+try:
+  with Controller.from_port(port = p) as controller:
+    ## Authentication not necessary when using Control Port Filter Proxy.
+    #controller.authenticate("password")
 
-  bootstrap_status = controller.get_info("status/bootstrap-phase")
+    bootstrap_status = controller.get_info("status/bootstrap-phase")    
+    
+    ## Possible answer, if network cable has been removed:
+    ## 250-status/bootstrap-phase=WARN BOOTSTRAP PROGRESS=80 TAG=conn_or SUMMARY="Connecting to the Tor network" WARNING="No route to host" REASON=NOROUTE COUNT=26 RECOMMENDATION=warn
+    
+    ## Possible answer:
+    ## 250-status/bootstrap-phase=NOTICE BOOTSTRAP PROGRESS=85 TAG=handshake_or SUMMARY="Finishing handshake with first hop"
+
+    ## Possible answer, when done:
+    ## 250-status/bootstrap-phase=NOTICE BOOTSTRAP PROGRESS=100 TAG=done SUMMARY="Done"
+    
+    ## TODO: parse the messages above.
+    
+    print "%s" % (bootstrap_status)
  
-  b = bootstrap_status.split( );  
-  c = ''.join(b[3]) 
-  d = c.split('='); 
-  e = d[1];
+    b = bootstrap_status.split( )
+    
+    #c = ''.join(b[3])
+    #d = c.split('=')
+    #e = d[1]
+    #print "%s" % (e)
 
-  print "%s" % (e)
-
-  progress = b[2];
+    progress = b[2]
   
-  progress_percent = ( progress.split( "=" ) )[1];
+    progress_percent = ( progress.split( "=" ) )[1]
   
-  #print "progress_percent: %s" % (progress_percent)
+    #print "progress_percent: %s" % (progress_percent)
 
-  sys.exit(int(progress_percent));
-
+    exit_code=int(progress_percent)
+    
+except:
+  exit_code=255
+  
+sys.exit(exit_code)
