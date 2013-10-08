8910eadd888160f9161c74a1d7af80e383a88c9f
adrelanos <adrelanos at riseup dot net>
2013-10-08 08:18:25 +0000

    new whonix_shared/etc/profile.d/20_default_browser.sh: set $BROWSER environment variable #86

---
+++ whonix_shared/etc/profile.d/20_default_browser.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This file is part of Whonix
+# Copyright (C) 2012 - 2013 adrelanos <adrelanos at riseup dot net>
+# See the file COPYING for copying conditions.
+
+## Whonix /etc/profile.d/20_default_browser.sh
+
+## /usr/bin/x-www-browser in Whonix will use  /usr/lib/whonix/confirm_open
+## (Whonix Confirm Open by default)
+##
+## See also:
+## The Default Browser on Linux Debacle
+## http://blog.codef00.com/2011/02/18/the-default-browser-on-linux-debacle/
+export BROWSER="/usr/bin/x-www-browser"
+
+## End of Whonix /etc/profile.d/20_default_browser.sh
