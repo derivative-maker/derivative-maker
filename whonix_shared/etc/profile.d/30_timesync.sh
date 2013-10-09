1dfd40277e7914385081e84cbadcfa84e484c320
adrelanos <adrelanos at riseup dot net>
2013-10-09 01:49:52 +0000

    refactoring #98

--- whonix_shared/etc/profile.d/30_timesync.sh
+++ whonix_shared/etc/profile.d/30_timesync.sh
@@ -9,8 +9,7 @@
 if [ ! "$(tty)" = "/dev/tty1" ]; then
    return 0
 else
-   ## The delay is just for the look and feel.
-   /usr/lib/whonix/delay sudo -u user /usr/lib/whonix/doutput --identifier timesync --icon /usr/share/whonix/icons/timesync.ico --showcli & disown
+   /usr/lib/whonix/timesync_profile_d & disown
 fi
 
 ## End of Whonix /etc/profile.d/30_timesync.sh
--- whonix_shared/etc/profile.d/40_whonixcheck.sh
+++ whonix_shared/etc/profile.d/40_whonixcheck.sh
@@ -9,8 +9,7 @@
 if [ ! "$(tty)" = "/dev/tty1" ]; then
    return 0
 else
-   ## The delay is just for the look and feel.
-   /usr/lib/whonix/delay sudo -u user /usr/lib/whonix/doutput --identifier whonixcheck --icon /usr/share/whonix/icons/whonix.ico --showcli & disown
+   /usr/lib/whonix/whonixcheck_profile_d & disown
 fi
 
 ## End of Whonix /etc/profile.d/40_whonixcheck.sh
