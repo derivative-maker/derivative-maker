13c18e4006acb3a75270ba54cd10451acbe13044
adrelanos <adrelanos at riseup dot net>
2013-12-11 19:34:23 +0000

    Whonix-Workstation: no longer install Tor Browser by default, this simplifies implementing verifiable builds (#113), installing iceweasel by default, which can be used to download Tor Browser, added local iceweasel browser homepage, the page itself has not been created yet

---
+++ whonix_workstation/etc/iceweasel/pref/whonix.js
@@ -0,0 +1,5 @@
+// This file is part of Whonix
+// Copyright (C) 2012 - 2013 adrelanos <adrelanos at riseup dot net>
+// See the file COPYING for copying conditions.
+
+pref("browser.startup.homepage","data:text/plain,browser.startup.homepage=/usr/share/whonix/iceweasel_local_homepage.html");
