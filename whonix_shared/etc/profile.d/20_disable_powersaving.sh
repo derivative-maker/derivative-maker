fc19d7b1d9810f605fb2411cfed09aa57fed96e0
Patrick Schleizer <adrelanos@riseup.net>
2014-04-10 03:51:46 +0000

    improved whonix_shared/etc/profile.d/20_disable_powersaving.sh

--- whonix_shared/etc/profile.d/20_disable_powersaving.sh
+++ whonix_shared/etc/profile.d/20_disable_powersaving.sh
@@ -4,9 +4,20 @@
 ## Copyright (C) 2012 - 2014 Patrick Schleizer <adrelanos@riseup.net>
 ## See the file COPYING for copying conditions.
 
-## Whonix /etc/profile.d/20_disable_powersaving.sh
+## /etc/profile.d/20_disable_powersaving.sh
 
 #set -x
+
+virt_what_command_exit_code="0"
+command -v virt-what >/dev/null || { virt_what_command_exit_code="$?" ; true; };
+
+if [ "$virt_what_command_exit_code" = "0" ]; then
+   true
+elif [ -x "/usr/sbin/virt-what" ]; then
+   true
+else
+   return 0
+fi
 
 result="$(sudo virt-what)"
 
@@ -22,10 +33,13 @@ fi
 ## and does not safe any energy. That is something the host should do, if
 ## wanted.
 
-user_name="user"
-setterm -blank 0 -powerdown 0
-sudo -u "$user_name" setterm -blank 0 -powerdown 0
+setterm -blank 0
+setterm -powerdown 0
+xset -dpms
+
+## Debugging.
+#xset -q
 
 #set +x
 
-## End of Whonix changes to /etc/profile.d/20_disable_powersaving.sh
+## End of /etc/profile.d/20_disable_powersaving.sh
