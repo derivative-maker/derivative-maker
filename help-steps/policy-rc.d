777833c95bc4c3275a1346370265d3960c27834d
Patrick Schleizer <adrelanos@riseup.net>
2015-03-13 21:46:13 +0000

    comment

--- help-steps/policy-rc.d
+++ help-steps/policy-rc.d
@@ -4,19 +4,17 @@
 ## Copyright (C) 2012 - 2014 Patrick Schleizer <adrelanos@riseup.net>
 ## See the file COPYING for copying conditions.
 
-## This file gets deleted by the vm script at the end.
+## Prevents daemons from starting while using apt-get.
+## Takes care of chroot mount from getting locked.
+
+## Therefore for example prevents connecting to the public Tor network while
+## building the images. This is interesting for (obfuscated) bridge users and
+## also prevents sensitive data from the build machine, such as the Tor
+## consensus /var/lib/tor leaking into the image.
+
+## This file gets deleted by the build script at the end.
 
 ## Thanks to
 ## http://lifeonubuntu.com/how-to-prevent-server-daemons-from-starting-during-apt-get-install/
-## Prevents daemons from starting while using apt-get.
-##
-## Therefore for example stops connecting to the public
-## Tor network while building the images.
-
-## This is interesting for (obfuscated) bridge users and
-## also prevents that senstive data from the build machine,
-## such as the Tor consensus can leak into /var/lib/tor.
-##
-## Should also take care of chroot mount getting locked.
 
 exit 101
