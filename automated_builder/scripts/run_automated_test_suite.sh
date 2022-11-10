d6bb111053b260e461518275a28eac755d7d7b6b
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2022-11-09 23:34:40 -0700

    Add initial setup for large VPS testing

---
+++ automated_builder/scripts/run_automated_test_suite.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+export ANSIBLE_VAULT_PASSWORD=$1
+export ANSIBLE_HOST_KEY_CHECKING=False
+source ./automated_builder/scripts/functions.bash
+
+main() {
+  decrypt_vault
+  run_automated_test_suite
+  encrypt_vault
+}
+
+run_builder() {
+  ansible-playbook -i automated_builder/inventory automated_builder/tasks/run_wats.yml
+}
+
+main
