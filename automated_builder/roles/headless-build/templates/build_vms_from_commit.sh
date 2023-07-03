7b659e290b49b0a777b167f3bd6c1391c54b7792
Patrick Schleizer <adrelanos@whonix.org>
2023-07-03 19:17:15 -0400

    CI

--- automated_builder/roles/headless-build/templates/build_vms_from_commit.sh
+++ automated_builder/roles/headless-build/templates/build_vms_from_commit.sh
@@ -8,6 +8,7 @@ export dist_build_no_unset_xtrace=true
 main() {
   build_gateway_vm >> /home/ansible/gateway_build.log 2>&1
   build_workstation_vm >> /home/ansible/workstation_build.log 2>&1
+  prepare_release >> /home/ansible/prepare_release.log 2>&1
 }
 
 build_gateway_vm() {
@@ -24,6 +25,12 @@ build_workstation_vm() {
     --target virtualbox \
     --remote-derivative-packages true \
     --allow-untagged true
+}
+
+prepare_release() {
+  dm-prepare-release \
+    --flavor whonix-workstation-xfce \
+    --target virtualbox
 }
 
 main
