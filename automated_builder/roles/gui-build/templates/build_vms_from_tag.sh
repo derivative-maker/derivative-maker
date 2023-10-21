a096e189595f9cdb01eeedd0c32c2cd5adcbe408
Patrick Schleizer <adrelanos@whonix.org>
2023-10-21 07:06:45 -0400

    CI

--- automated_builder/roles/gui-build/templates/build_vms_from_tag.sh
+++ automated_builder/roles/gui-build/templates/build_vms_from_tag.sh
@@ -2,9 +2,13 @@
 
 export dist_build_non_interactive=true
 
+## Debugging.
+#export dist_build_no_unset_xtrace=true
+
 main() {
   build_gateway_vm >> /home/ansible/gateway_build.log 2>&1
   build_workstation_vm >> /home/ansible/workstation_build.log 2>&1
+  prepare_release >> /home/ansible/prepare_release.log 2>&1
 }
 
 build_gateway_vm() {
@@ -16,6 +20,22 @@ build_gateway_vm() {
 
 build_workstation_vm() {
   /home/ansible/derivative-maker/derivative-maker \
+    --flavor whonix-workstation-xfce \
+    --target virtualbox \
+    --target windows
+}
+
+prepare_release() {
+  echo "TODO: Needs test keys for gpg and signify."
+  return 0
+
+  ## Does nothing but good to test anyhow.
+  dm-prepare-release \
+    --flavor whonix-gateway-xfce \
+    --target virtualbox \
+    --target windows
+
+  dm-prepare-release \
     --flavor whonix-workstation-xfce \
     --target virtualbox \
     --target windows
