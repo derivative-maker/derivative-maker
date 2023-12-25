05d48eb5475d0d797df73d941d5f888f4bf01e1f
Patrick Schleizer <adrelanos@whonix.org>
2023-12-25 16:17:56 -0500

    CI

--- automated_builder/roles/headless-build/templates/build_vms_from_commit.sh
+++ automated_builder/roles/headless-build/templates/build_vms_from_commit.sh
@@ -9,6 +9,7 @@ export CI=true
 
 main() {
   signing_key_test >> /home/ansible/signing_key_test.log 2>&1
+  build_kicksecure_iso_amd64 >> /home/ansible/kicksecure_iso_amd64_build.log 2>&1
   build_kicksecure_vm_arm64 >> /home/ansible/kicksecure_arm64_build.log 2>&1
   build_gateway_vm >> /home/ansible/gateway_build.log 2>&1
   build_workstation_vm >> /home/ansible/workstation_build.log 2>&1
@@ -20,9 +21,16 @@ signing_key_test() {
   /home/ansible/derivative-maker/help-steps/signing-key-test
 }
 
-build_kicksecure_vm_arm64() {
-  /home/ansible/derivative-maker/help-steps/signing-key-create
+build_kicksecure_iso_amd64() {
+  /home/ansible/derivative-maker/derivative-maker \
+    --flavor kicksecure-xfce \
+    --target iso \
+    --arch amd64 \
+    --remote-derivative-packages true \
+    --repo true
+}
 
+build_kicksecure_vm_arm64() {
   /home/ansible/derivative-maker/derivative-maker \
     --flavor kicksecure-xfce \
     --target utm \
