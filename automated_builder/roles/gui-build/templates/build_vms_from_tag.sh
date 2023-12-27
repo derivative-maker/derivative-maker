761a44fcb77fb93af5632db48841efab1a2b5e6b
Patrick Schleizer <adrelanos@whonix.org>
2023-12-27 08:22:17 -0500

    CI

--- automated_builder/roles/gui-build/templates/build_vms_from_tag.sh
+++ automated_builder/roles/gui-build/templates/build_vms_from_tag.sh
@@ -8,17 +8,17 @@ export CI=true
 #export dist_build_no_unset_xtrace=true
 
 main() {
-  signing_key_test >> /home/ansible/signing_key_test.log 2>&1
-  build_kicksecure_iso_amd64 >> /home/ansible/kicksecure_iso_amd64_build.log 2>&1
-  build_kicksecure_vm_arm64 >> /home/ansible/kicksecure_arm64_build.log 2>&1
-  build_gateway_vm >> /home/ansible/gateway_build.log 2>&1
-  build_workstation_vm >> /home/ansible/workstation_build.log 2>&1
-  prepare_release >> /home/ansible/prepare_release.log 2>&1
+  signing_key_test "$@" >> /home/ansible/signing_key_test.log 2>&1
+  build_kicksecure_iso_amd64 "$@" >> /home/ansible/kicksecure_iso_amd64_build.log 2>&1
+  build_kicksecure_vm_arm64 "$@" >> /home/ansible/kicksecure_arm64_build.log 2>&1
+  build_gateway_vm "$@" >> /home/ansible/gateway_build.log 2>&1
+  build_workstation_vm "$@" >> /home/ansible/workstation_build.log 2>&1
+  prepare_release "$@" >> /home/ansible/prepare_release.log 2>&1
 }
 
 signing_key_test() {
-  /home/ansible/derivative-maker/help-steps/signing-key-create
-  /home/ansible/derivative-maker/help-steps/signing-key-test
+  /home/ansible/derivative-maker/help-steps/signing-key-create "$@"
+  /home/ansible/derivative-maker/help-steps/signing-key-test "$@"
 }
 
 build_kicksecure_iso_amd64() {
@@ -27,8 +27,7 @@ build_kicksecure_iso_amd64() {
     --flavor kicksecure-xfce \
     --target iso \
     --arch amd64 \
-    --remote-derivative-packages true \
-    --repo true
+    "$@"
 }
 
 build_kicksecure_vm_arm64() {
@@ -36,7 +35,8 @@ build_kicksecure_vm_arm64() {
     --repo true \
     --flavor kicksecure-xfce \
     --target utm \
-    --arch arm64
+    --arch arm64 \
+    "$@"
 }
 
 build_gateway_vm() {
@@ -45,7 +45,8 @@ build_gateway_vm() {
     --flavor whonix-gateway-xfce \
     --target virtualbox \
     --target qcow2 \
-    --target windows
+    --target windows \
+    "$@"
 }
 
 build_workstation_vm() {
@@ -54,19 +55,22 @@ build_workstation_vm() {
     --flavor whonix-workstation-xfce \
     --target virtualbox \
     --target qcow2 \
-    --target windows
+    --target windows \
+    "$@"
 }
 
 prepare_release() {
   dm-prepare-release \
     --flavor kicksecure-xfce \
     --target iso \
-    --arch amd64
+    --arch amd64 \
+    "$@"
 
   dm-prepare-release \
     --flavor kicksecure-xfce \
     --target utm \
-    --arch arm64
+    --arch arm64 \
+    "$@"
 
   ## Does nothing but good to test anyhow.
   dm-prepare-release \
@@ -74,14 +78,16 @@ prepare_release() {
     --target virtualbox \
     --target qcow2 \
     --target windows \
-    --arch amd64
+    --arch amd64 \
+    "$@"
 
   dm-prepare-release \
     --flavor whonix-workstation-xfce \
     --target virtualbox \
     --target qcow2 \
     --target windows \
-    --arch amd64
+    --arch amd64 \
+    "$@"
 }
 
-main
+main "$@"
