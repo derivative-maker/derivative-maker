f500639034539dc3fa8c26877fd7d2e087870097
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2025-02-19 06:31:41 -0700

    Check for apt-cacher-ng start with netcat

--- apple/entrypoint.sh
+++ apple/entrypoint.sh
@@ -4,4 +4,12 @@ set -e
 
 sudo /usr/sbin/apt-cacher-ng -c /etc/apt-cacher-ng &
 
+echo "Waiting for apt-cacher-ng to start..."
+
+while ! nc -z 127.0.0.1 3142; do
+  sleep 1
+done
+
+echo "apt-cacher-ng is up!"
+
 /home/builder/derivative-maker/derivative-maker --flavor whonix-gateway-xfce --target virtualbox --arch arm64 --allow-untagged true --debug
