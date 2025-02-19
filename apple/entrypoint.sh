624fd0989e3f4ec6a0ef458b0bf0ebe569bfe4cb
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2025-02-19 06:11:03 -0700

    Rework entrypoint script to run as builder

--- apple/entrypoint.sh
+++ apple/entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-/usr/sbin/apt-cacher-ng -c /etc/apt-cacher-ng &
+sudo /usr/sbin/apt-cacher-ng -c /etc/apt-cacher-ng &
 
-/bin/bash
+/home/builder/derivative-maker/derivative-maker --flavor whonix-gateway-xfce --target virtualbox --arch arm64 --allow-untagged true --debug
