512c6ca84f27f789e70b7c13ea4e52097b623e83
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2025-02-20 19:43:41 -0700

    Attempt installing grub with env var

--- apple/entrypoint.sh
+++ apple/entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-sleep 60
+sleep 10
 sudo --non-interactive /usr/sbin/apt-cacher-ng -c /etc/apt-cacher-ng
 
 echo "Waiting for apt-cacher-ng to start..."
-sleep 60
+sleep 10
 
 /home/builder/derivative-maker/derivative-maker --flavor whonix-gateway-xfce --target utm --arch arm64 --repo true --tb open --vmsize 15g --allow-untagged true --debug
