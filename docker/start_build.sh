18917d661a73f7ef44149a96fb0d60bf215340bf
tabletseeker <148603424+tabletseeker@users.noreply.github.com>
2025-06-01 10:51:10 +0000

    Clean up

--- docker/start_build.sh
+++ docker/start_build.sh
@@ -2,26 +2,36 @@
 
 set -e
 
-LOG_DIR="${HOME}/derivative-maker/docker/logs"
+SOURCE_DIR="${HOME}/derivative-maker"
+BINARY_DIR="${SOURCE_DIR}/derivative-binary"
+LOG_DIR="${BINARY_DIR}/logs"
 GIT_LOG="${LOG_DIR}/git.log"
 BUILD_LOG="${LOG_DIR}/build.log"
 
-mkdir "${HOME}/derivative-maker/derivative-binary"
-ln -s "${HOME}/derivative-maker/derivative-binary" "${HOME}/derivative-binary"
- 
-cd "${HOME}/derivative-maker"
+mkdir -p "${BINARY_DIR}" "${LOG_DIR}"
+ln -s "${BINARY_DIR}" "${HOME}/derivative-binary"
+
+cd "${SOURCE_DIR}"
+
 {
-  if [ -z "${TAG:-}" ]; then
-    TAG="master";
-  fi
-  git pull
-  if [ "${TAG}" != 'master' ]; then
-    git describe
-    git verify-tag "${TAG}"
-  fi
-  git verify-commit "${TAG}^{commit}"
-  git checkout --recurse-submodules "${TAG}"
-  git status
+if [ -z "${TAG:-}" ]; then
+
+	TAG="master";
+
+fi
+
+git pull
+
+if [ "${TAG}" != 'master' ]; then
+
+	git describe
+	git verify-tag "${TAG}"
+
+fi
+
+git verify-commit "${TAG}^{commit}"
+git checkout --recurse-submodules "${TAG}"
+git status
 } 2>&1 | tee -a -- "${GIT_LOG}"
 
-"${HOME}/derivative-maker/derivative-maker" "$@" | tee -a ${BUILD_LOG}
+"${SOURCE_DIR}/derivative-maker" "$@" | tee -a ${BUILD_LOG}
