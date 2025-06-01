3cb47239f03395d1c63c14b9cca8c43b7cce44d3
tabletseeker <148603424+tabletseeker@users.noreply.github.com>
2025-06-01 17:39:33 +0000

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -5,7 +5,7 @@ set -e
 BUILDER_VOLUME="$(dirname $PWD)"
 CACHER_VOLUME="$HOME/apt_cacher_mnt"
 IMG="derivative-maker/derivative-maker-docker"
-USER="user"
+ARGS=""
 
 sudo modprobe -a loop dm_mod
 
@@ -16,4 +16,4 @@ sudo docker run --name derivative-maker-docker -it --rm --privileged \
 	--env 'DERIVATIVE_APT_REPOSITORY_OPTS=' \
 	--volume ${BUILDER_VOLUME}:/home/user/derivative-maker \
 	--volume ${CACHER_VOLUME}:/var/cache/apt-cacher-ng ${IMG} \
-	/bin/bash -c  "/usr/bin/su ${USER} --preserve-environment --session-command '/usr/bin/start_build.sh'"
+	/bin/bash -c  "/usr/bin/su ${USER} --preserve-environment --session-command '/usr/bin/start_build.sh ${ARGS}'"
