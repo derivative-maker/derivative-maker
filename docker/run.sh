fff4f2fc67f5d74639284c803f2cb0d5137277f2
Patrick Schleizer <adrelanos@whonix.org>
2025-06-06 11:57:36 -0400

    fix

--- docker/run.sh
+++ docker/run.sh
@@ -17,21 +17,31 @@ volume_check() {
   sudo -- chmod --recursive -- "${3}" "${1}"
 }
 
-while (( $# != 0 )); do
+## Thanks to:
+## http://mywiki.wooledge.org/BashFAQ/035
+
+while true; do
   case "$1" in
     -t|--tag)
       TAG="${2}"
       shift 2
     ;;
-    -*|--*)
-      ARGS+=("${@:1:2}")
-      shift 2
-    ;;
-    *)
+    --)
       shift
-    ;;
+      break
+      ;;
+    -*)
+      printf '%s\n' "$0: ERROR: unknown option: $1" >&2
+      return 0
+      ;;
+    *)
+      break
+      ;;
   esac
 done
+
+## If there are input files (for example) that follow the options, they
+## will remain in the "$@" positional parameters.
 
 volume_check "${CACHER_VOLUME}" '101:102' '770'
 
@@ -52,4 +62,4 @@ sudo -- \
   --volume "${BUILDER_VOLUME}:/home/user/derivative-maker" \
   --volume "${CACHER_VOLUME}:/var/cache/apt-cacher-ng" "${IMG}" \
   /bin/bash -c \
-  "/usr/bin/su ${USER} --preserve-environment --session-command '/usr/bin/start_build.sh ${ARGS[@]}'"
+  "/usr/bin/su ${USER} --preserve-environment --session-command '/usr/bin/start_build.sh ${@}'"
