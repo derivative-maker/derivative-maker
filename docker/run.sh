c45bda128b7bb1b7f19c95318af842db03c9b817
Patrick Schleizer <adrelanos@whonix.org>
2025-06-06 11:38:04 -0400

    refactoring

--- docker/run.sh
+++ docker/run.sh
@@ -7,30 +7,30 @@ CACHER_VOLUME="$HOME/apt_cacher_mnt"
 IMG="derivative-maker/derivative-maker-docker"
 
 volume_check() {
-	[ -d "${1}" ] || { \
-		mkdir -p "${1}"; sleep .1; \
-		sudo chown -R "${2}" "${1}"; \
-		sudo chmod -R "${3}" "${1}"; \
-	}
+  [ -d "${1}" ] || { \
+    mkdir -p "${1}"; sleep .1; \
+    sudo chown -R "${2}" "${1}"; \
+    sudo chmod -R "${3}" "${1}"; \
+  }
 }
 
 while (( $# != 0 )); do
 
-	case $1 in
+  case $1 in
 
-	-t|--tag)
-	TAG="${2}"
-	shift 2
-	;;
-	-*|--*)
-	ARGS+=("${@:1:2}")
-	shift 2
-	;;
-	*)
- 	shift
-	;;
+  -t|--tag)
+  TAG="${2}"
+  shift 2
+  ;;
+  -*|--*)
+  ARGS+=("${@:1:2}")
+  shift 2
+  ;;
+  *)
+   shift
+  ;;
 
-	esac
+  esac
 
 done
 
@@ -39,10 +39,10 @@ volume_check "${CACHER_VOLUME}" '101:102' '770'
 sudo modprobe -a loop dm_mod
 
 sudo docker run --name derivative-maker-docker -it --rm --privileged \
-	--env "TAG=${TAG}" \
- 	--env 'flavor_meta_packages_to_install=' \
-	--env 'install_package_list=' \
-	--env 'DERIVATIVE_APT_REPOSITORY_OPTS=' \
-	--volume ${BUILDER_VOLUME}:/home/user/derivative-maker \
-	--volume ${CACHER_VOLUME}:/var/cache/apt-cacher-ng ${IMG} \
-	/bin/bash -c  "/usr/bin/su ${USER} --preserve-environment --session-command '/usr/bin/start_build.sh ${ARGS[@]}'"
+  --env "TAG=${TAG}" \
+   --env 'flavor_meta_packages_to_install=' \
+  --env 'install_package_list=' \
+  --env 'DERIVATIVE_APT_REPOSITORY_OPTS=' \
+  --volume ${BUILDER_VOLUME}:/home/user/derivative-maker \
+  --volume ${CACHER_VOLUME}:/var/cache/apt-cacher-ng ${IMG} \
+  /bin/bash -c  "/usr/bin/su ${USER} --preserve-environment --session-command '/usr/bin/start_build.sh ${ARGS[@]}'"
