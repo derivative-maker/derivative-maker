5f2918d4dfbdd675d800372ffe7fec30c7c36662
Patrick Schleizer <adrelanos@whonix.org>
2025-06-06 13:49:54 -0400

    todo

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -36,6 +36,7 @@ printf '%s\n' "${quoted_args}" | tee -- /etc/docker-entrypoint-cmd >/dev/null
 chmod +x /etc/docker-entrypoint-cmd
 
 true "INFO: Create file: /etc/systemd/system/docker-entrypoint.service"
+## TODO: Consider making docker-entrypoint.service a standalone file.
 cat > /etc/systemd/system/docker-entrypoint.service <<EOF
 [Unit]
 Description=docker-entrypoint.service
@@ -43,6 +44,7 @@ Description=docker-entrypoint.service
 [Service]
 ExecStartPre=/bin/bash -e -x -c "cat -- /etc/docker-entrypoint-cmd"
 ExecStart=/bin/bash -e -x -c /etc/docker-entrypoint-cmd
+## TODO: Consider making ExecStopPost a standalone script.
 # EXIT_STATUS is either an exit code integer or a signal name string, see systemd.exec(5)
 ExecStopPost=/bin/bash -ec "if echo \${EXIT_STATUS} | grep [A-Z] > /dev/null; then echo >&2 \"got signal \${EXIT_STATUS}\"; systemctl exit \$(( 128 + \$( kill -l \${EXIT_STATUS} ) )); else systemctl exit \${EXIT_STATUS}; fi"
 StandardInput=tty-force
