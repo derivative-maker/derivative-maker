b09e233eb11ea6dbaaf44f993f73167734599138
Patrick Schleizer <adrelanos@whonix.org>
2025-06-06 13:50:41 -0400

    fix

--- docker/build.sh
+++ docker/build.sh
@@ -6,8 +6,8 @@
 set -x
 set -e
 
-MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MYDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd "$MYDIR"
+cd -- "$MYDIR"
 
 sudo -- docker build -t derivative-maker/derivative-maker-docker:latest .
