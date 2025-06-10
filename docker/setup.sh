703385369f79c788cd84c699543106b5b36a098f
tabletseeker <148603424+tabletseeker@users.noreply.github.com>
2025-06-10 03:22:54 -0400

    Creating setup.sh

---
+++ docker/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## Copyright (C) 2025 - 2025 ENCRYPTED SUPPORT LLC <adrelanos@whonix.org>
+## See the file COPYING for copying conditions.
+
+set -x
+set -e
+
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install \
+--no-install-recommends \
+--yes \
+dbus gpg dbus-user-session ca-certificates git time curl lsb-release fakeroot \
+dpkg-dev fasttrack-archive-keyring safe-rm adduser sudo apt-cacher-ng
+
+adduser --quiet --disabled-password --home "${HOME}" --gecos "${USER},,,," "${USER}"
+printf '%s\n' "${USER} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/passwordless_sudo
+chmod 440 /etc/sudoers.d/passwordless_sudo
+
+apt-get clean
+safe-rm -r -f -- /var/lib/apt/lists/* /var/cache/apt/*
