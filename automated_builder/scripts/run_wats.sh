c2f0c0b12eab87464d4be7d9609d13d38551a9eb
Patrick Schleizer <adrelanos@whonix.org>
2025-07-14 01:14:17 -0400

    sudo --non-interactive

--- automated_builder/scripts/run_wats.sh
+++ automated_builder/scripts/run_wats.sh
@@ -10,8 +10,8 @@ main() {
 }
 
 prepare_environment() {
-  echo changeme | sudo -S apt-get update -q
-  sudo apt-get install git python3-behave python3-pip python3-pyatspi -yq
+  echo changeme | sudo --non-interactive -S apt-get update -q
+  sudo --non-interactive -- apt-get install git python3-behave python3-pip python3-pyatspi -yq
   pip3 install dogtail -q
   gsettings set org.gnome.desktop.interface toolkit-accessibility true
 }
