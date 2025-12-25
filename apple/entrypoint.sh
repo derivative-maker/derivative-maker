6bc6a0e4b1a3773b5602c66562bee714a7c0996d
Patrick Schleizer <adrelanos@whonix.org>
2025-12-25 11:50:57 -0500

    CI

--- apple/entrypoint.sh
+++ apple/entrypoint.sh
@@ -9,4 +9,4 @@ sudo --non-interactive /usr/sbin/approx
 echo "Waiting for approx to start..."
 sleep 10
 
-/home/builder/derivative-maker/derivative-maker --flavor whonix-gateway-lxqt --target utm --arch arm64 --repo true --tb open --vmsize 15g --allow-untagged true --debug
+/home/builder/derivative-maker/derivative-maker --flavor whonix-gateway-lxqt --target virtualbox --arch arm64 --repo true --tb open --vmsize 15g --allow-untagged true --debug
